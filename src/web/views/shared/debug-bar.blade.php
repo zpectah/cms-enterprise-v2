326e902ce6fe1704e836e990312c2eab5fb74bc2
Tomas Sychra <sychrat@gmail.com>
2022-04-18 09:34:05 +0200

    updates

--- src/web/views/shared/debug-bar.blade.php
+++
@@ -1,7 +0,0 @@
-@if($public['web_mode_debug'])
-    <div
-        class="header-bar"
-    >
-        {{$t('debug:label')}}
-    </div>
-@endif
--- src/web/views/shared/maintenance-bar.blade.php
+++
@@ -1,7 +0,0 @@
-@if($public['web_mode_maintenance'])
-    <div
-        class="header-bar"
-    >
-        {{$t('maintenance:label')}}
-    </div>
-@endif
